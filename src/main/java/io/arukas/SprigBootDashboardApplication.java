50cdfec55fbc9c28297f40ec113211f39d3ea495
niuyuxian <ncc0706@gmail.com>
2018-10-23 17:53:02 +0800

    init

---
+++ src/main/java/io/arukas/SprigBootDashboardApplication.java
@@ -0,0 +1,12 @@
+package io.arukas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SprigBootDashboardApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SprigBootDashboardApplication.class, args);
+    }
+}
---
+++ src/test/java/io/arukas/SprigBootDashboardApplicationTests.java
@@ -0,0 +1,16 @@
+package io.arukas;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SprigBootDashboardApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
